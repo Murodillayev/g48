b2db13b77d04c9bbecd34f611a31a51eb580775e
Muhammadkomil <muhammadkomil.realsoft@gmail.com>
2024-11-01 16:55:26 +0500

    my first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Muhammadkomil Murodillayev
+ * @created 01/11/2024 - 16:00
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
