c3a4a9fa8fe17d3f26c6abe303653703e52510ce
Muhammadkomil <muhammadkomil.realsoft@gmail.com>
2024-11-01 17:30:42 +0500

    ssjdha

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,13 @@ public class Main {
         System.out.println("");
         System.out.println("");
         System.out.println("");
+
+
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
         System.out.println("");
         System.out.println("");
         System.out.println("");
